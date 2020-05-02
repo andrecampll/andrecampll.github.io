9c585dd82bc3cb60df11b217db76ac714aff5f3c
André Victor <andrevictor50@gmail.com>
2020-05-02 19:27:04 -0300

    Updates

---
+++ precache-manifest.a0ee4f01812308c7a4c0bccc5f894f6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a05cb3f070d4dd3930071f798bb331b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aec1069389d4f6cc8f3f",
+    "url": "/static/js/2.96954aba.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.96954aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7a35c6d7db9f134278",
+    "url": "/static/js/main.8f6e2ae1.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "482359a3882b5e5d364eaa9de6fbb563",
+    "url": "/static/media/gofinances.482359a3.png"
+  },
+  {
+    "revision": "ca12aca0e2990e9f5069f3aa7a3ef276",
+    "url": "/static/media/wallp.ca12aca0.jpg"
+  }
+]);
