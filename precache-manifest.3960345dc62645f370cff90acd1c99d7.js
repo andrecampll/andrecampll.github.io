3f80a2d21f75260e75647dc301e807420e1fd96f
André <andrevictor50@gmail.com>
2020-08-15 22:05:53 -0300

    Updates

---
+++ precache-manifest.3960345dc62645f370cff90acd1c99d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6046ce83dc2cd8a6848c563aeaac851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a17e2f6c0f5d57201639",
+    "url": "/static/js/2.725f0abd.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/static/js/2.725f0abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d7bfd79d2be33fc96f",
+    "url": "/static/js/main.ca2161a5.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4684c493ee467f0349862369fdfd558c.js"
+  "/precache-manifest.3960345dc62645f370cff90acd1c99d7.js"
 );
 
 self.addEventListener('message', (event) => {
